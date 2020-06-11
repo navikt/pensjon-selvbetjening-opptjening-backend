c476875296431130f12dd53006a6e6001b4290ce
R154508 <sindre.rasmussen@nav.no>
2020-06-11 13:12:51 +0200

    PL-1441: Added yml-files and ReadinessEndpoint.

---
+++ src/main/java/no/nav/pensjon/selvbetjeningopptjening/ReadinessEndpoint.java
@@ -0,0 +1,21 @@
+package no.nav.pensjon.selvbetjeningopptjening;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/internal")
+public class ReadinessEndpoint {
+    @RequestMapping(path = "isAlive", method = RequestMethod.GET)
+    public ResponseEntity isAlive() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "isReady", method = RequestMethod.GET)
+    public ResponseEntity isReady() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
