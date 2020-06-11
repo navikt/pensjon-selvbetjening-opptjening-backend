6a2b16279ad7261366115043f99135a94630922d
R154508 <sindre.rasmussen@nav.no>
2020-06-11 10:29:59 +0200

    PL-1441: Initial setup of application

---
+++ src/main/java/no/nav/pensjon/selvbetjeningopptjening/SelvbetjeningOpptjeningApplication.java
@@ -0,0 +1,13 @@
+package no.nav.pensjon.selvbetjeningopptjening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelvbetjeningOpptjeningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelvbetjeningOpptjeningApplication.class, args);
+    }
+
+}
