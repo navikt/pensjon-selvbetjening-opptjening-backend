ad887622709ac771cf36648c614ff4bd7f101eb8
Swati Sharma <swati.sharma@nav.no>
2020-08-26 08:18:59 +0200

    PL-1441 Add 1 test to check ArsakCode

--- src/test/java/no/nav/pensjon/selvbetjeningopptjening/opptjening/EndringPensjonsbeholdningCalculatorTest.java
+++ src/test/java/no/nav/pensjon/selvbetjeningopptjening/opptjening/EndringPensjonsbeholdningCalculatorTest.java
@@ -2,21 +2,33 @@ package no.nav.pensjon.selvbetjeningopptjening.opptjening;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import static no.nav.pensjon.selvbetjeningopptjening.model.code.TypeArsakCode.INNGAENDE;
+import static no.nav.pensjon.selvbetjeningopptjening.model.code.TypeArsakCode.OPPTJENING;
+import static no.nav.pensjon.selvbetjeningopptjening.model.code.TypeArsakCode.UTTAK;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import no.nav.pensjon.selvbetjeningopptjening.model.Beholdning;
 
 class EndringPensjonsbeholdningCalculatorTest {
 
+    @Mock
+    EndringPensjonsbeholdningCalculator endringPensjonsbeholdningCalculator;
+
+    @BeforeEach
+    public void setUp(){ endringPensjonsbeholdningCalculator = new EndringPensjonsbeholdningCalculator(); }
+
     @Test
     void when_empty_input_then_calculateEndringPensjonsbeholdning_returns_null_list() {
         List<EndringPensjonsopptjeningDto> dtos =
-                new EndringPensjonsbeholdningCalculator().calculateEndringPensjonsbeholdning(2020, new ArrayList<>(), new ArrayList<>());
+                endringPensjonsbeholdningCalculator.calculateEndringPensjonsbeholdning(2020, new ArrayList<>(), new ArrayList<>());
 
         assertNull(dtos);
     }
@@ -29,22 +41,24 @@ class EndringPensjonsbeholdningCalculatorTest {
         List<Beholdning> list = Collections.singletonList(beholdning);
 
         List<EndringPensjonsopptjeningDto> dtos =
-                new EndringPensjonsbeholdningCalculator().calculateEndringPensjonsbeholdning(2020, list, new ArrayList<>());
+                endringPensjonsbeholdningCalculator.calculateEndringPensjonsbeholdning(2020, list, new ArrayList<>());
 
         assertEquals(2, dtos.size());
     }
 
     @Test
-    void when_beholdning_list_has_one_element_with_FomDato_1JanGivenYear_then_calculateEndringPensjonsbeholdning_returns_3_elements() {
+    void when_FomDato_1JanGivenYear_then_addInngaendeBeholdning_and_addNyOpptjening_returns_3_ArsakType_values() {
         Beholdning beholdning = new Beholdning();
         beholdning.setFomDato(LocalDate.of(2020, 1, 1));
         beholdning.setBelop(1d);
         List<Beholdning> list = Collections.singletonList(beholdning);
 
         List<EndringPensjonsopptjeningDto> dtos =
-                new EndringPensjonsbeholdningCalculator().calculateEndringPensjonsbeholdning(2020, list, new ArrayList<>());
+                endringPensjonsbeholdningCalculator.calculateEndringPensjonsbeholdning(2020, list, new ArrayList<>());
 
-        assertEquals(3, dtos.size());
+        assertEquals(INNGAENDE, dtos.get(0).getArsakType());
+        assertEquals(OPPTJENING,dtos.get(1).getArsakType());
+        assertEquals(UTTAK,dtos.get(2).getArsakType());
     }
 
     @Test
@@ -56,7 +70,7 @@ class EndringPensjonsbeholdningCalculatorTest {
         List<Beholdning> list = Collections.singletonList(beholdning);
 
         List<EndringPensjonsopptjeningDto> dtos =
-                new EndringPensjonsbeholdningCalculator().calculateEndringPensjonsbeholdning(2020, list, new ArrayList<>());
+                endringPensjonsbeholdningCalculator.calculateEndringPensjonsbeholdning(2020, list, new ArrayList<>());
 
         assertEquals(4, dtos.size());
     }
