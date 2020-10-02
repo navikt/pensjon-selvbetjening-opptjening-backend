9b0c504165709681e7237e9c9df5bafe7eaa445e
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2020-10-02 14:39:55 +0200

    PL-1441 Add test for LocalDateTimeFromEpochDeserializer

---
+++ src/test/java/no/nav/pensjon/selvbetjeningopptjening/util/LocalDateFromEpochDeserializerTest.java
@@ -0,0 +1,27 @@
+package no.nav.pensjon.selvbetjeningopptjening.util;
+
+import com.fasterxml.jackson.core.JsonParser;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+class LocalDateFromEpochDeserializerTest {
+
+    @Mock
+    JsonParser parser;
+
+    @Test
+    void deserialize_gets_localDate_from_epoch() throws IOException {
+        when(parser.readValueAs(Long.class)).thenReturn(1600000000000L);
+        LocalDate date = new LocalDateTimeFromEpochDeserializer().deserialize(parser, null);
+        assertEquals(LocalDate.of(2020, 9, 13), date);
+    }
+}
