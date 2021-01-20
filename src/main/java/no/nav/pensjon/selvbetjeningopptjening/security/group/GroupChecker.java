9dfa2644287fb909136194a8692732c2545ca4fa
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2021-01-20 11:07:19 +0100

    PL-2906 Use same authorized AAD groups as in PSELV
    Epic PL-1441

--- src/main/java/no/nav/pensjon/selvbetjeningopptjening/security/group/GroupChecker.java
+++ src/main/java/no/nav/pensjon/selvbetjeningopptjening/security/group/GroupChecker.java
@@ -5,15 +5,16 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 import static java.util.Objects.requireNonNull;
-import static no.nav.pensjon.selvbetjeningopptjening.security.group.Groups.*;
+import static no.nav.pensjon.selvbetjeningopptjening.security.group.AadGroupIds.*;
 
 @Component
 public class GroupChecker {
 
+    // The list of groups is the same as the internal user groups in
+    // github.com/navikt/pesys/blob/master/pselv/presentation/nav-presentation-pensjon-pselv-web-core/src/main/resources/menu_pensjon.xml#L66
     private static final List<String> AUTHORIZED_GROUPS = List.of(
-            BEGRENSET_VEILEDER,
             BRUKERHJELPA,
-            KUNDESENTER,
+            OKONOMI,
             SAKSBEHANDLER,
             VEILEDER);
 
