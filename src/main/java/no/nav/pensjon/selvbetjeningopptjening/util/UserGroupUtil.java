652617351a459734de0c2c3760920e3b9de53db1
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2021-01-29 10:42:00 +0100

    Add metric for user group

--- src/main/java/no/nav/pensjon/selvbetjeningopptjening/util/UserGroupUtil.java
+++ src/main/java/no/nav/pensjon/selvbetjeningopptjening/util/UserGroupUtil.java
@@ -1,5 +1,6 @@
 package no.nav.pensjon.selvbetjeningopptjening.util;
 
+import io.micrometer.core.instrument.Metrics;
 import no.nav.pensjon.selvbetjeningopptjening.model.code.UserGroup;
 
 import java.time.LocalDate;
@@ -17,23 +18,33 @@ public class UserGroupUtil {
 
     private static UserGroup findUserGroup(int year, Month month) {
         if (year < FIRST_BIRTHYEAR_WITH_NEW_ALDER) {
+            incrementCounter("Født før 1954");
             return USER_GROUP_1;
         }
 
         if (year <= FIRST_BIRTHYEAR_WITH_NEW_AFP) {
+            incrementCounter("Født før 1954");
+
             return year == FIRST_BIRTHYEAR_WITH_NEW_AFP && month.equals(DECEMBER)
                     ? USER_GROUP_3
                     : USER_GROUP_2;
         }
 
         if (year < FIRST_BIRTHYEAR_WITH_OVERGANGSREGLER) {
+            incrementCounter("Født før 1954");
             return USER_GROUP_3;
         }
 
         if (year <= LAST_BIRTHYEAR_WITH_OVERGANGSREGLER) {
+            incrementCounter("Født 1954-1962");
             return USER_GROUP_4;
         }
 
+        incrementCounter("Født etter 1962");
         return USER_GROUP_5;
+    }
+
+    private static void incrementCounter(String group) {
+        Metrics.counter("pensjon_selvbetjening_user_group_counter", "user-group", group).increment();
     }
 }
