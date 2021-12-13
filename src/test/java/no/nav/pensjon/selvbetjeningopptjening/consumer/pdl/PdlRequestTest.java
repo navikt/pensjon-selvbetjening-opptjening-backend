e1ce6500088b1c66d59be52be42a98094f59117a
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2021-12-13 16:13:45 +0100

    PL-4840 Use org.json.JSONObject
    Instead of Spring Boot's configurationprocessor.json.JSONObject,
    which is missing in Java 17 image used.
    Epic PL-1441

--- src/test/java/no/nav/pensjon/selvbetjeningopptjening/consumer/pdl/PdlRequestTest.java
+++ src/test/java/no/nav/pensjon/selvbetjeningopptjening/consumer/pdl/PdlRequestTest.java
@@ -3,7 +3,6 @@ package no.nav.pensjon.selvbetjeningopptjening.consumer.pdl;
 import no.nav.pensjon.selvbetjeningopptjening.TestFnrs;
 import no.nav.pensjon.selvbetjeningopptjening.opptjening.Pid;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.configurationprocessor.json.JSONException;
 
 import java.io.IOException;
 
@@ -12,13 +11,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class PdlRequestTest {
 
     @Test
-    void getPersonQuery_should_return_graphQL_query_from_file_together_with_variables() throws JSONException, IOException {
+    void getPersonQuery_should_return_graphQL_query_from_file_together_with_variables() throws IOException {
         String query = PdlRequest.getPersonQuery(new Pid(TestFnrs.NORMAL));
 
-        assertEquals("{\"query\":\"query($ident: ID!){    hentPerson(ident: $ident){        navn(historikk: false){" +
-                "            fornavn            mellomnavn            etternavn            folkeregistermetadata {                " +
-                "ajourholdstidspunkt            }            metadata {                master                endringer {         " +
-                "           registrert                }            }        }        foedsel{            foedselsdato            " +
-                "foedselsaar        }    }}\",\"variables\":{\"ident\":\"03029119367\"}}", query);
+        assertEquals("{\"variables\":{\"ident\":\"03029119367\"}," +
+                "\"query\":\"query($ident: ID!){    hentPerson(ident: $ident){        navn(historikk: false){            fornavn            mellomnavn            etternavn            folkeregistermetadata {                ajourholdstidspunkt            }" +
+                "            metadata {                master                endringer {                    registrert                }            }        }        foedsel{            foedselsdato            foedselsaar        }    }}\"}",
+                query);
     }
 }
