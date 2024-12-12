d29f5c2965c05a02dcbf6a9d1a09cb4b2acf7c7a
Sæbø <gard.sebo@nav.no>
2024-12-12 10:21:20 +0100

    Sjekk på fullmakt og bruker selv er flyttet ut. Fjerner test

--- src/test/java/no/nav/pensjon/selvbetjeningopptjening/fullmakt/FullmaktFacadeTest.java
+++ src/test/java/no/nav/pensjon/selvbetjeningopptjening/fullmakt/FullmaktFacadeTest.java
@@ -16,7 +16,6 @@ class FullmaktFacadeTest {
 
     private static final String IRRELEVANT_PID = "01029345678";
     private static final String FULLMAKTSGIVER_PID = "04925398980";
-    private static final String FULLMEKTIG_PID = "30915399246";
     private FullmaktFacade facade;
 
     @Mock
@@ -31,11 +30,6 @@ class FullmaktFacadeTest {
     void mayActOnBehalfOf_isTrue_when_fullmektig_has_fullmaktsforhold() {
         when(service.fetchRepresentasjonsgyldighet(FULLMAKTSGIVER_PID)).thenReturn(new RepresentasjonValidity(true, "", "", ""));
         assertTrue(facade.mayActOnBehalfOf(FULLMAKTSGIVER_PID).hasValidRepresentasjonsforhold());
-    }
-
-    @Test
-    void mayActOnBehalfOf_isTrue_when_fullmektig_and_fullmaktsgiver_are_same_person() {
-        assertTrue(facade.mayActOnBehalfOf(IRRELEVANT_PID).hasValidRepresentasjonsforhold());
     }
 
     @Test
