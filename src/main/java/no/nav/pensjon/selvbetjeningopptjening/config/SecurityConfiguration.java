94e8473dd2ebdfc04a9623535c93467f045c0a96
Sæbø <gard.sebo@nav.no>
2025-02-26 09:34:47 +0100

    Lagt til pen app id

--- src/main/java/no/nav/pensjon/selvbetjeningopptjening/config/SecurityConfiguration.java
+++ src/main/java/no/nav/pensjon/selvbetjeningopptjening/config/SecurityConfiguration.java
@@ -21,18 +21,17 @@ public class SecurityConfiguration {
      */
     @Bean
     public TokenAudiencesVsApps tokenAudiencesVsApps(
-            @Value("${fss-gateway-app-id}") String fssGatewayAppId,
             @Value("${pensjon.representasjon.app-id}") String fullmaktAppId,
             @Value("${pdl.app-id}") String pdlAppId,
             @Value("${pensjonsopptjening-register-app-id}") String pensjonsopptjeningRegisterAppId,
-            @Value("${skjermede-personer-pip-app-id}") String skjermedePersonerPipAppId) {
+            @Value("${skjermede-personer-pip-app-id}") String skjermedePersonerPipAppId,
+            @Value("${pen.app-id}") String penAppId) {
         return new TokenAudiencesVsApps(
-                Map.of(fssGatewayAppId, List.of(
-                                AppIds.PENSJONSFAGLIG_KJERNE.appName),
-                        fullmaktAppId, List.of(AppIds.FULLMAKT.appName),
+                Map.of(fullmaktAppId, List.of(AppIds.FULLMAKT.appName),
                         pdlAppId, List.of(AppIds.PERSONDATALOSNINGEN.appName),
                         pensjonsopptjeningRegisterAppId, List.of(AppIds.PENSJONSOPPTJENING_REGISTER.appName),
-                        skjermedePersonerPipAppId, List.of(AppIds.SKJERMEDE_PERSONER_PIP.appName)));
+                        skjermedePersonerPipAppId, List.of(AppIds.SKJERMEDE_PERSONER_PIP.appName),
+                        penAppId, List.of(AppIds.PENSJONSFAGLIG_KJERNE.appName)));
     }
 
     @Bean
