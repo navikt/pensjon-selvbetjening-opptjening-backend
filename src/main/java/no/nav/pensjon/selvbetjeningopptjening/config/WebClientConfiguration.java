15cb3edea01b97352634a765323ca07874c8a716
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-10-06 13:46:10 +0200

    Use debug logging for WebClient
    Also clean application-local.properties

--- src/main/java/no/nav/pensjon/selvbetjeningopptjening/config/WebClientConfiguration.java
+++ src/main/java/no/nav/pensjon/selvbetjeningopptjening/config/WebClientConfiguration.java
@@ -1,15 +1,14 @@
 package no.nav.pensjon.selvbetjeningopptjening.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import io.netty.handler.logging.LogLevel;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 @Configuration
 public class WebClientConfiguration {
@@ -17,7 +16,10 @@ public class WebClientConfiguration {
     @Bean
     @Primary
     WebClient webClient() {
-        var httpClient = HttpClient.create().wiretap(true);
+        var httpClient = HttpClient.create()
+                .wiretap("reactor.netty.http.client.HttpClient",
+                        LogLevel.DEBUG,
+                        AdvancedByteBufFormat.TEXTUAL);
 
         return WebClient.builder()
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
@@ -27,7 +29,10 @@ public class WebClientConfiguration {
     @Bean
     @Qualifier("epoch-support")
     WebClient webClientWithEpochSupport() {
-        var httpClient = HttpClient.create().wiretap(true);
+        var httpClient = HttpClient.create().wiretap(
+                "reactor.netty.http.client.HttpClient",
+                LogLevel.DEBUG,
+                AdvancedByteBufFormat.TEXTUAL);
 
         return WebClient.builder()
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
